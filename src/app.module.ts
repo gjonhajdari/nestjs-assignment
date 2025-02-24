472e7607c69fdcd932e55b8f2c04f0b933a3702f
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-02-24 10:36:13 +0100

    Create resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ProjectsModule } from "./api/projects/projects.module";
+import { TasksModule } from "./api/tasks/tasks.module";
+import { UsersModule } from "./api/users/users.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-	imports: [],
+	imports: [UsersModule, ProjectsModule, TasksModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
