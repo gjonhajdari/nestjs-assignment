1b6247625d87ecc75d9671a17f7c6f9d76d31720
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-02-26 16:08:32 +0100

    Refactor code to add or remove users from project within one function

--- src/api/projects/projects.service.ts
+++ src/api/projects/projects.service.ts
@@ -8,6 +8,7 @@ import { omit } from "lodash";
 import { DbUtilsService } from "src/common/services/db-utils.service";
 import { Repository } from "typeorm";
 import { UsersService } from "../users/users.service";
+import { ManageProjectUsersDto } from "./dtos/manage-project-users.dto";
 import { UpdateProjectDto } from "./dtos/update-project.dto";
 import { Project } from "./project.entity";
 
@@ -88,6 +89,43 @@ export class ProjectsService {
 	}
 
 	/**
+	 * Manages adding into or removing users from a project
+	 *
+	 * @param projectId - The unique UUID of the project
+	 * @param userId - The unique UUID of the user
+	 * @param operation - Operation we want to perform ("add" or "remove")
+	 * @returns
+	 */
+	private async manageProjectUsers(
+		projectId: string,
+		userId: string,
+		operation: "add" | "remove",
+	): Promise<Project> {
+		const project = await this.findById(projectId, true);
+		const existsInProject = project.users.map((u) => u.id).includes(userId);
+
+		if (operation === "add") {
+			if (existsInProject)
+				throw new BadRequestException("User is already added to project");
+
+			const user = await this.usersService.findById(userId);
+			project.users.push(user);
+		}
+
+		if (operation === "remove") {
+			if (!existsInProject)
+				throw new BadRequestException("User doesn't exist in project");
+
+			const position = project.users.findIndex((u) => u.id === userId);
+			project.users.splice(position, 1);
+		}
+
+		return this.dbUtilsService.executeSafely(() =>
+			this.projectRepository.save(project),
+		);
+	}
+
+	/**
 	 * Adds a user to a specific project
 	 *
 	 * @param projectId The unique UUID of the project
@@ -95,19 +133,20 @@ export class ProjectsService {
 	 * @returns Promise that resolves to the added Project entity
 	 * @throws {BadRequestException} - If user is already added to the project
 	 */
-	async addUser(projectId: string, userId: string): Promise<Project> {
-		const project = await this.findById(projectId, true);
-		const user = await this.usersService.findById(userId);
+	async addUser(payload: ManageProjectUsersDto): Promise<Project> {
+		return this.manageProjectUsers(payload.projectId, payload.userId, "add");
+	}
 
-		if (project.users.map((u) => u.id).includes(userId)) {
-			throw new BadRequestException("User is already added to project");
-		}
-
-		project.users.push(user);
-
-		return this.dbUtilsService.executeSafely(() =>
-			this.projectRepository.save(project),
-		);
+	/**
+	 * Removes a user from a specific project
+	 *
+	 * @param projectId The unique UUID of the project
+	 * @param userId The unique UUID of the user
+	 * @returns Promise that resolves to the added Project entity
+	 * @throws {BadRequestException} - If user isn't part of the project
+	 */
+	async removeUser(payload: ManageProjectUsersDto): Promise<Project> {
+		return this.manageProjectUsers(payload.projectId, payload.userId, "remove");
 	}
 
 	/**
