ba8c1af4566f62b4c374ba60263999dba2f92f3a
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-02-26 10:39:03 +0100

    Add UserModule dependency to get UserService

--- src/api/projects/projects.module.ts
+++ src/api/projects/projects.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "../users/users.module";
 import { Project } from "./project.entity";
 import { ProjectsController } from "./projects.controller";
 import { ProjectsService } from "./projects.service";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Project])],
+	imports: [TypeOrmModule.forFeature([Project]), UsersModule],
 	controllers: [ProjectsController],
 	providers: [ProjectsService],
 	exports: [ProjectsService],
