3d938f8d950de83845ae188ebfa3970775334f18
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-02-26 17:35:24 +0100

    Make guard check the validity of an api key

--- src/api/guards/auth.guard.ts
+++ src/api/guards/auth.guard.ts
@@ -1,11 +1,19 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Inject } from "@nestjs/common";
 import { Request } from "express";
+import { ApiService } from "../auth/api.service";
 
 export class AuthGuard implements CanActivate {
-	canActivate(context: ExecutionContext) {
-		const req: Request = context.switchToHttp().getRequest();
+	constructor(@Inject() private apiService: ApiService) {}
 
-		if (!req.headers["x-api-key"]) return false;
+	async canActivate(context: ExecutionContext) {
+		const req: Request = context.switchToHttp().getRequest();
+		const providedKey = req.headers["x-api-key"] as string;
+
+		if (!providedKey) return false;
+
+		const key = await this.apiService.findKey(providedKey);
+
+		if (!key) return false;
 		return true;
 	}
 }
