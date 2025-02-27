f32cfc2491025554f8c18903ff7f334f1e40b0b4
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-02-27 13:57:56 +0100

    Configure testing environment

--- src/config/data-source.config.ts
+++ src/config/data-source.config.ts
@@ -6,20 +6,21 @@ dotenv.config({
 
 export const config = {
 	name: "default",
-	type: "postgres",
+	type: process.env.TYPEORM_TYPE,
 	host: process.env.TYPEORM_HOST,
 	port: process.env.TYPEORM_PORT || 5432,
 	username: process.env.TYPEORM_USERNAME,
 	password: process.env.TYPEORM_PASSWORD,
 	database: process.env.TYPEORM_DATABASE,
 	entities: [process.env.TYPEORM_ENTITIES],
-	synchronize: true,
-	dropSchema: false,
+	synchronize: process.env.TYPEORM_SYNCHRONIZE,
+	dropSchema: process.env.TYPEORM_DROP_SCHEMA,
+	migrationsRun: process.env.TYPEORM_MIGRATIONS_RUN,
 };
 
 export const configNoSynchronize = {
 	name: "default",
-	type: "postgres",
+	type: process.env.TYPEORM_TYPE,
 	host: process.env.TYPEORM_HOST,
 	port: process.env.TYPEORM_PORT || 5432,
 	username: process.env.TYPEORM_USERNAME,
